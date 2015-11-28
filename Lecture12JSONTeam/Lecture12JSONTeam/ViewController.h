eb776c6c320cdc3c7268a6b1f0cb193e9cf68fe2
Yevhen Herasymenko <fess2036@ya.ru>
2015-11-28 10:47:01 +0200

    feat: create project

---
+++ Lecture12JSONTeam/Lecture12JSONTeam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lecture12JSONTeam
+//
+//  Created by Yevhen Herasymenko on 28/11/2015.
+//  Copyright © 2015 YevhenHerasymenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Lecture12JSONTeam/Lecture12JSONTeam/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Lecture12JSONTeam
+//
+//  Created by Yevhen Herasymenko on 28/11/2015.
+//  Copyright © 2015 YevhenHerasymenko. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
